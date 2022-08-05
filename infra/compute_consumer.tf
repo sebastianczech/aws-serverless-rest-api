e1c7100b02d717ca8f4fd99a2110008ab6d15a94
Sebastian Czech <sebaczech@gmail.com>
2022-08-06 00:49:21 +0200

    Add permissions for DynamoDB

--- infra/compute_consumer.tf
+++ infra/compute_consumer.tf
@@ -31,7 +31,7 @@ resource "aws_iam_policy" "lambda_consumer_sqs_receive_iam_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "ConsumerStatement",
+      "Sid": "ConsumerStatementSqs",
       "Action": [
         "sqs:ReceiveMessage",
         "sqs:DeleteMessage",
@@ -48,19 +48,41 @@ EOF
 resource "aws_iam_policy" "lambda_consumer_sns_publish_iam_policy" {
   name        = "lambda_consumer_sns_publish_iam_policy"
   path        = "/"
-  description = "IAM policy for publish evento from Lambda to SNS"
+  description = "IAM policy for publish events from Lambda to SNS"
 
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "Stmt1659644631574",
+      "Sid": "ConsumerStatementSns",
       "Action": [
         "sns:Publish"
       ],
       "Effect": "Allow",
       "Resource": "${aws_sns_topic.cloud_sns_serverless_rest_api.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "lambda_consumer_dynamo_put_iam_policy" {
+  name        = "lambda_consumer_dynamo_put_iam_policy"
+  path        = "/"
+  description = "IAM policy for put items from Lambda to DynamoDB"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "ConsumerStatementSns",
+      "Action": [
+        "dynamodb:PutItem"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_dynamodb_table.cloud_dynamodb_serverless_rest_api.arn}"
     }
   ]
 }
@@ -75,6 +97,11 @@ resource "aws_iam_role_policy_attachment" "lambda_consumer_sqs" {
 resource "aws_iam_role_policy_attachment" "lambda_consumer_sns" {
   role       = aws_iam_role.lambda_consumer_role.name
   policy_arn = aws_iam_policy.lambda_consumer_sns_publish_iam_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_consumer_dynamo" {
+  role       = aws_iam_role.lambda_consumer_role.name
+  policy_arn = aws_iam_policy.lambda_consumer_dynamo_put_iam_policy.arn
 }
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping_sqs_lambda_consumer" {
