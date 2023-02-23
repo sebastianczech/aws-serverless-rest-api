2bf40bdd45589473bfe54b9507f8f137a803d75a
Sebastian Czech <sebaczech@gmail.com>
2023-02-23 14:15:21 +0100

    Decrease RCU and WCU for DynamoDB

--- infra/database.tf
+++ infra/database.tf
@@ -2,8 +2,8 @@ resource "aws_dynamodb_table" "localstack_dynamodb_serverless_rest_api" {
   count          = var.create_services_on_localstack ? 1 : 0
   provider       = aws.localstack
   name           = "localstack_dynamodb_serverless_rest_api"
-  read_capacity  = "10"
-  write_capacity = "10"
+  read_capacity  = "1"
+  write_capacity = "1"
   hash_key       = "ID"
 
   attribute {
@@ -15,8 +15,8 @@ resource "aws_dynamodb_table" "localstack_dynamodb_serverless_rest_api" {
 resource "aws_dynamodb_table" "cloud_dynamodb_serverless_rest_api" {
   provider       = aws.cloud
   name           = "cloud_dynamodb_serverless_rest_api"
-  read_capacity  = "10"
-  write_capacity = "10"
+  read_capacity  = "1"
+  write_capacity = "1"
   hash_key       = "ID"
 
   attribute {
