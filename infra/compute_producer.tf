a21d55e200f85520a4703d071b89748c90800675
Sebastian Czech <sebaczech@gmail.com>
2023-05-19 21:27:30 +0200

    Use new check{} block available from TF 1.5-beta

--- infra/compute_producer.tf
+++ infra/compute_producer.tf
@@ -151,3 +151,17 @@ resource "aws_lambda_permission" "allow_iam_user" {
 #   function_url_auth_type = "NONE"
 #   principal              = "*"
 # }
+
+check "lambda_deployed" {
+  data "external" "this" {
+    program = ["curl", "${aws_lambda_function_url.lambda_producer_endpoint.function_url}"]
+  }
+
+  assert {
+    # If we execution function using URL without authentication, then it should be received forbidden message, if Lambda is deployed correctly
+    condition = data.external.this.result.Message == "Forbidden"
+    error_message = format("The Lambda %s is not deployed.",
+      aws_lambda_function.lambda_producer.function_name
+    )
+  }
+}
